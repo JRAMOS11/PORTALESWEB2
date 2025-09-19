4b79e700ce2e8cbf373a696e41a5370403c786e8
Jasiel Ramos <jaramo230@unicah.edu>
2025-09-19 00:37:59 -0600

    Commit inicial - Portalesweb2

---
+++ java/script.js
@@ -0,0 +1,7 @@
+// Mensaje en consola
+console.log("Portafolio cargado correctamente.");
+
+// Ejemplo: alerta al cargar la página
+window.onload = function() {
+    alert("¡Bienvenido al portafolio de Jasiel!");
+};
